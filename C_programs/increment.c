4962c27569e47fb922008994f9d9edbc500cc2dd
M.Geetha Vardhani <geethavardhani.m@gmail.com>
2026-07-15 15:24:50 +0000

    increment

---
+++ C_programs/increment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a=9;
+    printf("The value of a is %d\n",a);
+        printf("The value of a is %d\n",++a);
+    printf("The value of a is %d\n",a++);
+    printf("The value of a is %d\n",--a);
+    printf("The value of a is %d\n",a--);
+    printf("The value of a is %d\n",a);
+
+return 0;
+}

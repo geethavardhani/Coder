7b9a9f2883c4ca84c1093bc8e3f94dea50244381
M.Geetha Vardhani <geethavardhani.m@gmail.com>
2026-07-15 15:22:05 +0000

    do while loop

---
+++ C_programs/do while loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    do
+    {
+        printf("Enter the value of n : \n");
+        scanf("%d", &n);
+        printf("\n");
+    } while (n != 0);
+
+    printf("");
+    return 0;
+}
